502a42ef9ce6f4101b937904cf63c7602c662c6a
Ankur-2003 <2020bteit00059@walchandsangli.ac.in>
2022-06-17 22:31:51 +0530

    version 0.0.1 base app

---
+++ app/src/main/java/com/myinnovation/customer/Activity/Main.java
@@ -0,0 +1,18 @@
+package com.myinnovation.customer.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseAuth;
+import com.myinnovation.customer.R;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
