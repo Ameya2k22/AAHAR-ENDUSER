f29cb38063e78557802c2ecff2479110086afe97
AnkitaZade03 <ankitazade2003@gmail.com>
2022-06-19 16:16:15 +0530

    Attendance

--- app/src/main/java/com/myinnovation/customer/ui/Attendance/AttendanceFragment.java
+++ app/src/main/java/com/myinnovation/customer/ui/Attendance/AttendanceFragment.java
@@ -9,6 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -16,11 +18,16 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.myinnovation.customer.Customer;
 import com.myinnovation.customer.R;
 import com.myinnovation.customer.User;
 import com.myinnovation.customer.databinding.FragmentAttendanceBinding;
 
 public class AttendanceFragment extends Fragment {
+
+    private RadioGroup radioGroup;
+    private RadioButton radioButton1, radioButton2, rButton;
+    private Button submit;
 
     public AttendanceFragment() {
         // Required empty public constructor
@@ -33,25 +40,80 @@ public class AttendanceFragment extends Fragment {
 
     }
     private FragmentAttendanceBinding binding;
-    User user;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        Button yes, no;
         binding = FragmentAttendanceBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
 
-        yes = view.findViewById(R.id.yes);
-        no = view.findViewById(R.id.no);
+        radioGroup = view.findViewById(R.id.attendance);
+        radioButton1 = view.findViewById(R.id.yes);
+        radioButton2 = view.findViewById(R.id.no);
+        submit = view.findViewById(R.id.submit);
 
-        yes.setEnabled(false);
-        no.setEnabled(false);
+        radioButton1.setEnabled(false);
+        radioButton2.setEnabled(false);
+        submit.setEnabled(false);
 
-        Toast.makeText(getActivity(), "Hello World", Toast.LENGTH_SHORT).show();
+
+        FirebaseDatabase.getInstance().getReference("EndUser").child("Details").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot snapshot1: snapshot.getChildren()){
+                    if(snapshot1.getKey().equals(FirebaseAuth.getInstance().getUid())){
+                        User user = snapshot1.getValue(User.class);
+                        String id = user.getMess_id();
+
+                        FirebaseDatabase.getInstance().getReference("Customer").child("Attendance").child(id).child("SessionOn").addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                boolean ans = snapshot.getValue(Boolean.class);
+                                if(ans){
+                                    radioButton1.setEnabled(true);
+                                    radioButton2.setEnabled(true);
+                                    submit.setEnabled(true);
 
 
+                                    submit.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View view) {
+                                            int selectedID = radioGroup.getCheckedRadioButtonId();
+                                            rButton =  (RadioButton) view.findViewById(selectedID);
+
+                                            if(selectedID == -1){
+
+                                            }
+                                            else{
+                                                Toast.makeText(getActivity(), rButton.getText().toString(), Toast.LENGTH_SHORT).show();
+                                            }
+
+                                        }
+                                    });
+                                }
+                                else{
+                                    radioButton1.setEnabled(false);
+                                    radioButton2.setEnabled(false);
+                                    submit.setEnabled(false);
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         return view;
     }
--- app/src/main/java/com/myinnovation/customer/ui/home/HomeFragment.java
+++ app/src/main/java/com/myinnovation/customer/ui/home/HomeFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +16,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
@@ -22,9 +24,11 @@ import com.google.firebase.database.ValueEventListener;
 import com.myinnovation.customer.Adapter.HomeAdapter;
 import com.myinnovation.customer.Customer;
 import com.myinnovation.customer.R;
+import com.myinnovation.customer.User;
 import com.myinnovation.customer.databinding.FragmentHomeBinding;
 
 import java.util.ArrayList;
+import java.util.StringJoiner;
 
 public class HomeFragment extends Fragment {
 
@@ -59,7 +63,30 @@ public class HomeFragment extends Fragment {
         FirebaseDatabase.getInstance().getReference().child("EndUser").child("Details").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for(DataSnapshot snapshot1:snapshot)
+                for(DataSnapshot snapshot1:snapshot.getChildren()){
+                    if(snapshot1.getKey().equals(FirebaseAuth.getInstance().getUid())){
+                        User user = snapshot1.getValue(User.class);
+                        String id = user.getMess_id();
+
+                        FirebaseDatabase.getInstance().getReference().child("Customer").child("Mess-Info").addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                for(DataSnapshot snapshot2:snapshot.getChildren()){
+                                    if(snapshot2.getKey().equals(id)){
+                                        Customer customer = snapshot2.getValue(Customer.class);
+                                        joinedMessList.add(customer);
+                                    }
+                                }
+                                homeAdapter.notifyDataSetChanged();
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+                    }
+                }
             }
 
             @Override
