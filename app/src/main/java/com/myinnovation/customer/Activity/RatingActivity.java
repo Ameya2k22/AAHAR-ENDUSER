33164ac6e8bad7bd4480d359b183bf2f2437b4bf
raptor-2001 <pratham007yadav@gmail.com>
2022-06-21 17:59:00 +0530

    Updated optimized UI

--- app/src/main/java/com/myinnovation/customer/Activity/RatingActivity.java
+++ app/src/main/java/com/myinnovation/customer/Activity/RatingActivity.java
@@ -1,6 +1,7 @@
 package com.myinnovation.customer.Activity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,10 +10,34 @@ import com.myinnovation.customer.databinding.ActivityRatingBinding;
 public class RatingActivity extends AppCompatActivity {
 
     ActivityRatingBinding binding;
+    private float ratingValue = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityRatingBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        // get rating value from database and set to rating bar.
+
+        binding.editRatingBtn.setOnClickListener(v -> {
+            ratingValue = binding.ratingBar.getRating();
+            Toast.makeText(RatingActivity.this, "Rating = " + ratingValue, Toast.LENGTH_SHORT).show();
+        });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // check whether user joined in this mess or not.
+
+        // if yes
+        binding.ratingBar.setEnabled(true);
+        binding.editRatingBtn.setEnabled(true);
+        // else
+//        binding.ratingBar.setEnabled(false);
+//        binding.editRatingBtn.setEnabled(false);
     }
 }
